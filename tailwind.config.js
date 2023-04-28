1c96e22451669cc9f3e5a609ecba10cac4a1d618
Tebb <howard.taylor@olymnos.com>
2023-04-28 17:13:13 +0100

    add tailwindcss + inter font

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  }, plugins: [],
+}
